93e09101542044fbe211fc3a7d2bcbef65261cda
Aaron de Mello <aaron.d@nylas.com>
2021-08-04 09:31:07 -0400

    fix: commit

--- packages/aarondm-test-package-1/index.js
+++ packages/aarondm-test-package-1/index.js
@@ -2,3 +2,4 @@ console.log("Major refactor");
 console.log("Testing without conventional commits");
 console.log("Okay testing another");
 console.log("Okay testing another x2");
+console.log("Okay testing another x3");
