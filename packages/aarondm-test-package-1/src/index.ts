9792b6d76df8d6b6336f4dcc78ff977e32db0c80
AaronDDM <aaron.d@nylas.com>
2021-08-04 16:43:45 -0400

    console.log("2.0.3-canary.7");

--- packages/aarondm-test-package-1/src/index.ts
+++ packages/aarondm-test-package-1/src/index.ts
@@ -1,4 +1,4 @@
 import { SOME_CONST } from "aarondm-test-common/index";
 
-console.log("2.0.3-canary.6");
+console.log("2.0.3-canary.7");
 console.log(SOME_CONST);
--- packages/aarondm-test-package-2/src/index.ts
+++ packages/aarondm-test-package-2/src/index.ts
@@ -1,4 +1,4 @@
 import { newFunction } from "aarondm-test-common/index"
 
-console.log("2.0.3-canary.6");
+console.log("2.0.3-canary.7");
 console.log(newFunction())
