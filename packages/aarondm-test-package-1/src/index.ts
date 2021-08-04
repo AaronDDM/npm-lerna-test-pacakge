b24ae1a629cf61889e1fdd9359515635ff443694
AaronDDM <aaron.d@nylas.com>
2021-08-04 16:40:48 -0400

    console.log("2.0.3-canary.6");

--- packages/aarondm-test-package-1/src/index.ts
+++ packages/aarondm-test-package-1/src/index.ts
@@ -1,11 +1,4 @@
 import { SOME_CONST } from "aarondm-test-common/index";
 
-console.log("Major refactor");
-console.log("Testing without conventional commits");
-console.log("Okay testing another");
-console.log("Okay testing another x2");
-console.log("Okay testing another x3");
-console.log("Okay testing another x4");
-console.log("Okay testing another x5");
-console.log("Okay testing another x6");
+console.log("2.0.3-canary.6");
 console.log(SOME_CONST);
--- packages/aarondm-test-package-2/src/index.ts
+++ packages/aarondm-test-package-2/src/index.ts
@@ -1,4 +1,4 @@
 import { newFunction } from "aarondm-test-common/index"
 
-console.log("Test package trigger build")
+console.log("2.0.3-canary.6");
 console.log(newFunction())
