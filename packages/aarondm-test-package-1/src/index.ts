3f04dca04695b82590f73ff85d5425ec1ca8c311
AaronDDM <aaron.d@nylas.com>
2021-08-04 18:10:42 -0400

    9

--- packages/aarondm-test-package-1/src/index.ts
+++ packages/aarondm-test-package-1/src/index.ts
@@ -1,4 +1,4 @@
 import { SOME_CONST } from "@aaronddm/core";
 
-console.log("2.0.3-canary.8");
+console.log("2.0.3-canary.9");
 console.log(SOME_CONST);
--- packages/aarondm-test-package-2/src/index.ts
+++ packages/aarondm-test-package-2/src/index.ts
@@ -1,4 +1,4 @@
 import { newFunction } from "@aaronddm/core"
 
-console.log("2.0.3-canary.8");
+console.log("2.0.3-canary.9");
 console.log(newFunction())
