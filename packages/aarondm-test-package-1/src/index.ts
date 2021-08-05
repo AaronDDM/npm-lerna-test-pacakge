392646e2fb01072c040a6309ed0c471d0f5d8fc5
AaronDDM <aaron.d@nylas.com>
2021-08-04 20:17:34 -0400

    Added a new func

--- packages/aarondm-test-package-1/src/index.ts
+++ packages/aarondm-test-package-1/src/index.ts
@@ -1,4 +1,5 @@
-import { SOME_CONST } from "aaronddm-core";
+import { newFunction2, SOME_CONST } from "aaronddm-core";
 
 console.log("2.0.3-canary.9");
 console.log(SOME_CONST);
+console.log(newFunction2());
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,3 +3,6 @@ export const SOME_CONST = 'common update';
 export function newFunction() {
     return 'update-test-3';
 }
+export function newFunction2() {
+    return 'Brand new function imported';
+}
